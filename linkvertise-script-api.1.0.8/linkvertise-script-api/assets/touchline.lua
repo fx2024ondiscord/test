9e37ae840e8cf955251e555fd3df29073e95947d
Some_But!TOOL <143144389+fx2024ondiscord@users.noreply.github.com>
2025-11-20 09:19:08 -1200

    Update script URL in touchline.lua

--- linkvertise-script-api.1.0.8/linkvertise-script-api/assets/touchline.lua
+++ linkvertise-script-api.1.0.8/linkvertise-script-api/assets/touchline.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/4bcc25e1b1d24a818f97c4224a09b517"))()
+loadstring(game:HttpGet("https://loadstringer.overhub.xyz/scripts/69166cf5ca37854a62ecc5a8"))()
