ad8cb8a4504060046a3bc0ec2852e8cf66a4e8a6
Some_But!TOOL <143144389+fx2024ondiscord@users.noreply.github.com>
2025-11-20 12:02:13 -1200

    Update touchline.lua

--- linkvertise-script-api.1.0.8/linkvertise-script-api/assets/touchline.lua
+++ linkvertise-script-api.1.0.8/linkvertise-script-api/assets/touchline.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://loadstringer.overhub.xyz/scripts/69166cf5ca37854a62ecc5a8"))()
+loadstring(game:HttpGet("https://loadstringer.overhub.xyz/scripts/68eaaa9c592ef5a745c65299"))()
